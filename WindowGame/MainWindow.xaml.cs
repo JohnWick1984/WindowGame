53c6a0fc54ecaf7ee7be583f2718c23fec72f47b
Евгений <@EUGENE1984>
2023-12-16 16:22:04 +0500

    Добавьте файлы проекта.

---
+++ WindowGame/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows;
+
+namespace NumberGuesserWPF
+{
+    public partial class MainWindow : Window
+    {
+        private NumberGuesser game;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            game = new NumberGuesser();
+            StartNewGame();
+        }
+
+        private void StartNewGame()
+        {
+            game.PlayGame();
+            GuessText.Text = "Догадка: ";
+        }
+
+        private void GuessedButtonClick(object sender, RoutedEventArgs e)
+        {
+            GuessText.Text = $"Поздравляем! Вы угадали число {game.TargetNumber} за {game.Attempts} попыток.";
+            DisableButtons();
+        }
+
+        private void NotGuessedButtonClick(object sender, RoutedEventArgs e)
+        {
+            GuessText.Text = $"Загаданное число: {game.TargetNumber}. Попробуйте еще раз.";
+            DisableButtons();
+        }
+
+        private void RestartButtonClick(object sender, RoutedEventArgs e)
+        {
+            StartNewGame();
+            EnableButtons();
+        }
+
+        private void DisableButtons()
+        {
+            // Деактивация кнопок "Угадал" и "Не угадал"
+            GuessedButton.IsEnabled = false;
+            NotGuessedButton.IsEnabled = false;
+        }
+
+        private void EnableButtons()
+        {
+            // Активация кнопок "Угадал" и "Не угадал"
+            GuessedButton.IsEnabled = true;
+            NotGuessedButton.IsEnabled = true;
+        }
+    }
+}
